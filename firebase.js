2aed6bed3463cb0f332f1e821c5e9fab8ca02ea9
Agostinho Facitela <agostinhofacitela2006@gmail.com>
2025-07-07 14:33:10 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,24 +1,12 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import dotenv from "dotenv";
+const admin = require("firebase-admin");
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
-// Carrega variáveis do .env
-dotenv.config();
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-export { db };
+const db = admin.firestore();
+module.exports = db;
 
 
 
