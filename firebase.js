14e5ceef75a8299c3bbf405790575b1056c80bfd
Agostinho Facitela <agostinhofacitela2006@gmail.com>
2025-07-07 14:21:46 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,5 +1,9 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import dotenv from "dotenv";
+
+// Carrega variáveis do .env
+dotenv.config();
 
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
@@ -12,6 +16,9 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+export { db };
+
 
 
